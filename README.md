7310a2c98685c4f9f94e2c7f9c87898c8bf68b40
garbaz <garbaz@t-online.de>
2022-05-04 21:51:43 +0200

    added save key & download key

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 
 A simple offline in-browser notepad. Contents are automatically saved on closing the page. 
 
-Require a reasonably moder browser, since it uses `window.localStorage` for saving.
-
-Pressing `TAB` toggles the editability of the text field. 
+Require a reasonably modern browser, since it uses `window.localStorage` for saving.
 
 _Note:_ The contents are saved in the browser and are tied to the URL, so if you open the notepad in a different browser or with a different path, the old contents will no longer be visible, though they aren't gone! Just open the notepad again with the old URL.
+
+## Usage
+
+Pressing `Tab` toggles the editability of the text field.
+
+Pressing `Ctrl+S` saves the contents. This is pretty much redundant, since saving occurs automatically upon the tab being closed. This just exists so that I don't keep accidentally opening the save dialogue of the browser out of habit. 
+
+Pressing `Ctrl+D` downloads the current contents as a text file.
--- notepad.js
+++ notepad.js
@@ -1,24 +1,81 @@
 (function() {
 var notepad = null;
+var notepad_edited = false;
+
+const edited_status = function(edited) {
+    notepad_edited = edited;
+    if (notepad_edited) {
+        document.title = '(*)Notepad';
+    } else {
+        document.title = 'Notepad';
+    }
+};
+
+const save_notepad_contents = function() {
+    window.localStorage.setItem('notepad_contents', notepad.innerHTML);
+    edited_status(false);
+};
+
 window.addEventListener('load', function() {
     notepad = document.getElementById('notepad');
 
     notepad.innerHTML = window.localStorage.getItem('notepad_contents');
 
     window.onbeforeunload = function() {
-        window.localStorage.setItem('notepad_contents', notepad.innerHTML);
+        save_notepad_contents();
     };
+
+    new MutationObserver(function(_, _) {
+        edited_status(true);
+    }).observe(notepad, {
+        subtree: true,
+        characterData: true,
+        childList: true,
+    });
 });
 
 window.addEventListener('keydown', function(ev) {
-    if (!ev.altKey && !ev.ctrlKey && !ev.metaKey && ev.key == 'Tab') { // Tab
+    if (ev.key == 'Tab' && (!ev.altKey && !ev.ctrlKey && !ev.metaKey && !ev.shiftKey)) {
+        ev.preventDefault();
+
         if (notepad.contentEditable == 'false') {
             notepad.contentEditable = 'true';
         } else {
             notepad.contentEditable = 'false';
         }
+    }
 
+    if ((ev.ctrlKey && ev.key == 's') && (!ev.altKey && !ev.metaKey && !ev.shiftKey)) {
         ev.preventDefault();
+
+        save_notepad_contents();
+    }
+
+    if ((ev.ctrlKey && ev.key == 'd') && (!ev.altKey && !ev.metaKey && !ev.shiftKey)) {
+        ev.preventDefault();
+
+        const date = new Date(Date.now()).toISOString().replace(/:/g, '-').slice(0, -5);
+        download(notepad.innerText, 'Notepad-' + date + '.txt', 'text/plain');
     }
 });
+
+// https://stackoverflow.com/questions/13405129/create-and-save-a-file-with-javascript
+// Function to download data to a file
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob)  // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else {  // Others
+        var a = document.createElement('a'),
+            url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
 })();
